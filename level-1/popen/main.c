6bbb0fedc5db9c41dd6f0177422e70c76a35ed5a
Byte206 <gabimagister@outlook.es>
2026-01-20 21:54:56 +0100

    IM THAT FUCKING PENGUING GOING TO THE MOUNTAIN

--- level-1/popen/main.c
+++ level-1/popen/main.c
@@ -5,28 +5,27 @@
 int ft_popen(const char *file, char *const av[], int type)
 {
 	if (!file || !av || (type != 'r' && type != 'w'))
-		return(-1);
+		return (-1);
+
 	int fd[2];
-	if(pipe(fd) < 0)
+	if (pipe(fd) < 0)
 		return (-1);
 	pid_t pid = fork();
 	if (pid < 0)
 	{
 		close(fd[1]);
 		close(fd[0]);
-		return(-1);
+		return (-1);
 	}
-	if(pid == 0)
+	if (pid == 0)
 	{
-		if (type == 'r')
+		if(type == 'r')
 		{
-			close(fd[0]);
 			if (dup2(fd[1], 1) < 0)
 				exit(-1);
 		}
 		else
 		{
-			close(fd[1]);
 			if(dup2(fd[0], 0) < 0)
 				exit(-1);
 		}
@@ -35,14 +34,17 @@ int ft_popen(const char *file, char *const av[], int type)
 		execvp(file, av);
 		exit(-1);
 	}
-	if (type == 'r')
+	else
 	{
-		close(fd[1]);
-		return(fd[0]);
+		if (type == 'r')
+		{
+			close(fd[1]);
+			return (fd[0]);
+		}
+		else
+		close(fd[0]);
+		return(fd[1]);
 	}
-	close(fd[0]);
-	return(fd[1]);
-
 }
 
 
